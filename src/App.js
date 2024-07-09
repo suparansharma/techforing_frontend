bd0c56a4e9d1363715851f18c7a2708c5345356c
suparansharma <suparansharmap@gmail.com>
2024-07-09 20:57:56 +0600

    login and register done successfully

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import AuthUser from './components/AuthUser';
+import Guest from './components/navbar/guest';
+import Auth from './components/navbar/auth';
 function App() {
+  const {getToken} = AuthUser();
+  if(!getToken()){
+    return <Guest />
+  }
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">MyApp</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-
-
-
-    </div>
-
+      <Auth />
   );
 }
 
--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import AuthUser from "../components/AuthUser";
 
-const Dashbord = () => {
+export default function Dashboard() {
+  const { http } = AuthUser();
+  const [userdetail, setUserdetail] = useState('');
+
+  useEffect(() => {
+    fetchUserDetail();
+  }, []);
+
+  const fetchUserDetail = () => {
+    http.post('/me').then((res) => {
+      console.log(res.data)
+      setUserdetail(res.data);
+    });
+  }
+
+  function renderElement() {
+    if (userdetail) {
+      return <div>
+        <h4>Name</h4>
+        <p>{userdetail.name}</p>
+        <h4>Email</h4>
+        <p>{userdetail.email}</p>
+      </div>
+    } else {
+      return <p>Loading.....</p>
+    }
+
+  }
+
   return (
-    <div>Dashbord</div>
+    <div>
+      <h1 className='mb-4 mt-4'>Dashboard page</h1>
+      {renderElement()}
+    </div>
   )
 }
-
-export default Dashbord
